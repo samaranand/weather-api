64e3c1ea4b804e9b8f0e159a1e25b0e9cb54b5e9
samaranand <samarss2015@gmail.com>
2021-06-02 02:04:22 +0530

    try-2

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -3,7 +3,9 @@ const request = require('request')
 const forecast = (latitude, longitude, callback) => {
     // console.log(longitude)
     // console.log(latitude)
-    const url = 'http://api.weatherstack.com/current?access_key=b89d8003de07c26f531113ce63058c56&query=' + latitude + ',' + longitude
+    const _url = 'http://api.weatherstack.com/current?access_key=b89d8003de07c26f531113ce63058c56&query=' + latitude + ',' + longitude
+    const key = `e6a856597d8d762f3b46bce3db408178`
+    const url = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=minutely,hourly&appid=${key}`
 
     request({ url, json: true }, (error, { body }) => {
         if (error) {
@@ -12,7 +14,10 @@ const forecast = (latitude, longitude, callback) => {
             callback('Unable to find location', undefined)
         } else {
             // console.log(body.current)
-            callback(undefined, (body.current.weather_descriptions[0] + ' It is currently ' + body.current.temperature + ' degress out. There is a ' + body.current.humidity + ' humidity.'))
+            // console.log(url)
+            callback(undefined, body)
+            // callback('nonde', undefined)
+            // callback(undefined, (body.current.weather_descriptions[0] + ' It is currently ' + body.current.temperature + ' degress out. There is a ' + body.current.humidity + ' humidity.'))
         }
     })
 }
